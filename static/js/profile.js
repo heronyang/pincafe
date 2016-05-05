8306e2c407916cbe3a3757816eb0e52d3b8777f7
Heron Yang <heron.yang.tw@gmail.com>
2016-05-05 17:10:24 +0800

    update

--- static/js/profile.js
+++ static/js/profile.js
@@ -29,10 +29,40 @@ function initMap() {
   });
 
 }
-$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
-(function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/Bgp8YMt9eyVHT7XyHTOAO5zRHBxsvcwJuF44owJE/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
 
-var switchTo5x=true;
+function setupParse() {
+
+  Parse.initialize('XJFp6TdDwyF3T');
+  Parse.serverURL = 'https://pincafe-parse.herokuapp.com/';
+  var Cafe = Parse.Object.extend("Cafe");
+  var query = new Parse.Query(Cafe);
+  // query.equalTo("name", "picnic");
+  query.find({
+      success: function(cafes) {
+        console.log("success", cafes.length);
+        for(var i = 0; i < cafes.length; i ++) {
+          var cafe = cafes[i];
+          var toDisplayContent = [cafe.get("name"), cafe.get("description"), cafe.get("address")];
+          console.log(toDisplayContent);
+        }
+      },
+      error: function(object, error) {
+        console.log("fail");
+      }
+  });
+
+}
+
+function setupLightBox() {
+
+  $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+  (function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/Bgp8YMt9eyVHT7XyHTOAO5zRHBxsvcwJuF44owJE/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
+
+}
+
+setupLightBox();
+setupParse();
